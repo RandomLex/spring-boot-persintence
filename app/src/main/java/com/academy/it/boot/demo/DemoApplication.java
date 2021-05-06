49fc9d2927d02b0d844986dd948c3f24658cab45
Alexej_Barzykin <Alexej_Barzykin@epam.com>
2021-05-06 20:48:08 +0300

    03-spring-boot-added-spring-security

--- app/src/main/java/com/academy/it/boot/demo/DemoApplication.java
+++ app/src/main/java/com/academy/it/boot/demo/DemoApplication.java
@@ -4,8 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @SpringBootApplication
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class DemoApplication {
 
 	public static void main(String[] args) {
---
+++ app/src/main/java/com/academy/it/boot/demo/config/SecurityConfig.java
@@ -0,0 +1,52 @@
+package com.academy.it.boot.demo.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final UserDetailsService userService;
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/", "/index.html").permitAll()
+                .antMatchers("/employees").authenticated()
+                .antMatchers("/info").hasAnyRole("ADMIN")
+//                .antMatchers("/info").hasAuthority("INFO")
+//                .antMatchers("/info").hasAnyAuthority("INFO", "ROLE_ADMIN")
+                .antMatchers("/self").hasAnyRole("USER")
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic()
+                .and()
+                .logout().logoutSuccessUrl("/")
+                .and()
+                .csrf().disable();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        authenticationProvider.setUserDetailsService(userService);
+        return authenticationProvider;
+    }
+
+}
